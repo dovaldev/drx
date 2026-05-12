d77361efa8d3c3b242e0f00b3c6be6af4b53cdc3
J.A. Doval <73448899+dovaldev@users.noreply.github.com>
2026-05-13 00:41:01 +0200

    chore: update author and remove temporary scripts

--- test_jsx_before.ts
+++
@@ -1,5 +0,0 @@
-import ts from "typescript";
-const code = "const a = 1; return <div/>; const b = Array<T>; const c = <T>(x: T) => x; const d = a < b;";
-const sf = ts.createSourceFile("test.tsx", code, ts.ScriptTarget.Latest, true, ts.ScriptKind.TSX);
-const printer = ts.createPrinter();
-console.log(printer.printFile(sf));
--- test_regex.js
+++
@@ -1,3 +0,0 @@
-const str = "foo\n  bar\r\n  baz";
-console.log(JSON.stringify(str.replace(/\n\s*/g, " ")));
-console.log(JSON.stringify(str.replace(/\r?\n\s*/g, " ")));
